2ad75f3ed92d2f8b109a78e29e25e43b2b33da77
QuantumRaven <carpenterrchloe@protonmail.com>
2025-05-01 11:41:17 -0400

    Initial commit - Upload local repo to GitHub

---
+++ git/new-repo-origin.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+: <<"AUTHOR_NOTES"
+
+Author: Chloe Carpenter
+
+Purpose: Push existing repository from the CLI to GitHub
+
+AUTHOR_NOTES
+
+: <<"HANDLE_TRAPS"
+
+Handle trap function for error handling
+
+HANDLE_TRAPS
+
+handle_err() {
+
+  local s=$?
+  echo "$0:${BASH_LINENO[0]} $BASH_COMMAND"
+  exit $s
+
+}
+
+trap handle_err ERR
+
+main() {
+    
+    # local variables
+    
+    local ORIGIN_LINK
+
+    read -rep "Git origin link: " ORIGIN_LINK
+
+    git remote add origin ORIGIN_LINK
+    git branch -M main
+    git push -u origin main
+
+}
